0a44669daf4da9bb2855d2c12384d9ac2d268ba2
Ruben <ruben.stabel@student.kuleuven.be>
2023-05-22 23:09:59 +0200

    simple rule based agent without speed param

--- src/traffic_simulation/driving/simple_rule_based_self_driving.py
+++ src/traffic_simulation/driving/simple_rule_based_self_driving.py
@@ -7,16 +7,16 @@ def simple_rule_based_driving(player_car, pedestrian):
         player_car.move_forward()
         output = [1, 0, 0]
     else:
-        if -pedestrian.img.get_width() < pedestrian.x - player_car.x < 40 and player_car.y - IMAGE_DIM + player_car.IMG.get_height() < pedestrian.y:
-            if player_car.get_vel() > 0:
-                # print("CASE 2")
-                player_car.move_backward()
-                output = [0, 1, 0]
-            else:
-                # print("CASE 3")
-                player_car.reduce_speed()
-                output = [0, 0, 1]
-        elif -pedestrian.img.get_width() < pedestrian.x - player_car.x < 80 and player_car.y - IMAGE_DIM + player_car.IMG.get_height() < pedestrian.y:
+        if -pedestrian.img.get_width() < pedestrian.x - player_car.x < 50 and player_car.y - IMAGE_DIM + player_car.IMG.get_height() < pedestrian.y:
+            # if player_car.get_vel() > 0:
+            # print("CASE 2")
+            player_car.move_backward()
+            output = [0, 1, 0]
+            # else:
+            #     # print("CASE 3")
+            #     player_car.reduce_speed()
+            #     output = [0, 0, 1]
+        elif -pedestrian.img.get_width() < pedestrian.x - player_car.x < 100 and player_car.y - IMAGE_DIM + player_car.IMG.get_height() < pedestrian.y:
             # print("CASE 4")
             player_car.reduce_speed()
             output = [0, 0, 1]
