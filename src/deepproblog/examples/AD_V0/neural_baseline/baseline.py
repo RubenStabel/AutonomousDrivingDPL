2d2f2631fbf00ecd7b808098145dbcd5ab4a2afb
Ruben <ruben.stabel@student.kuleuven.be>
2023-05-25 01:52:37 +0200

    Create REAL basleine only working with PyTorch (no DPL)

--- src/deepproblog/examples/AD_V0/neural_baseline/baseline.py
+++ src/deepproblog/examples/AD_V0/neural_baseline/baseline.py
@@ -6,16 +6,23 @@ import torch.nn as nn
 import torch.optim as optim
 
 from baseline_models import AD_baseline_net
-from deepproblog.examples.AD_V0.data.AD_generate_datasets_V1 import train_dataset, valid_dataset
+from deepproblog.examples.AD_V0.data.AD_generate_datasets_V1 import get_dataset
+
+
+def correct(predictions, labels):
+    classes = torch.argmax(predictions, dim=1)
+    return torch.mean((classes == labels).float())
+
+train = get_dataset('train')
 
 transform = transforms.Compose(
     [transforms.ToTensor(),
      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-batch_size = 4
+batch_size = 2
 
-trainloader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size,
-                                          shuffle=True, num_workers=2)
+trainloader = torch.utils.data.DataLoader(train, batch_size=batch_size,
+                                          shuffle=True)
 
 # testloader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size,
 #                                          shuffle=False, num_workers=2)
@@ -25,31 +32,44 @@ classes = (0,1,2)
 net = AD_baseline_net()
 
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.95)
 
 if __name__ == '__main__':
     freeze_support()
-    for epoch in range(2):  # loop over the dataset multiple times
+    for epoch in range(20):  # loop over the dataset multiple times
 
         running_loss = 0.0
+        correct_var = 0
         for i, data in enumerate(trainloader, 0):
             # get the inputs; data is a list of [inputs, labels]
             inputs, labels = data
-            # print(labels)
 
             # zero the parameter gradients
             optimizer.zero_grad()
 
             # forward + backward + optimize
             outputs = net(inputs)
+
             loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
 
             # print statistics
             running_loss += loss.item()
-            if i % 20 == 0:    # print every 2000 mini-batches
-                print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
-                running_loss = 0.0
+            correct_var += correct(outputs, labels)
+            # for i in range(len(labels)):
+            #     correct += int(outputs == labels)
+
+
+            # trainset, not train_loader
+            # probably x in your case
+
+
+            # if i % 20 == 0:    # print every 2000 mini-batches
+            #     print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+            #     running_loss = 0.0
+
+        accuracy = correct_var / len(trainloader)
+        print("Accuracy = {}".format(accuracy))
 
 print('Finished Training')
--- src/deepproblog/examples/AD_V0/neural_baseline/baseline_models.py
+++ src/deepproblog/examples/AD_V0/neural_baseline/baseline_models.py
@@ -10,15 +10,17 @@ class AD_baseline_net(nn.Module):
         self.conv2 = nn.Conv2d(6, 16, 5)
         self.fc1 = nn.Linear(16 * 5 * 5, 120)
         self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        self.fc3 = nn.Linear(84, 3)
+        self.softmax = nn.Softmax(-1)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = torch.flatten(x, 1)  # flatten all dimensions except batch
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
+        x = self.softmax(x)
         return x
 
 class Separate_Baseline(nn.Module):
