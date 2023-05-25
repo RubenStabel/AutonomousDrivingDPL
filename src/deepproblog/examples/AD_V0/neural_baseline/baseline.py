514ca53b2946dfc5249cf6613254ee4a291485a8
Ruben <ruben.stabel@student.kuleuven.be>
2023-05-25 16:40:17 +0200

    add running accuracy, test accuracy and confusion matrix to baseline

--- src/deepproblog/examples/AD_V0/neural_baseline/baseline.py
+++ src/deepproblog/examples/AD_V0/neural_baseline/baseline.py
@@ -1,19 +1,28 @@
 from multiprocessing import freeze_support
 
+import numpy as np
 import torch
 import torchvision.transforms as transforms
 import torch.nn as nn
 import torch.optim as optim
+from matplotlib import pyplot as plt
 
-from baseline_models import AD_baseline_net
+from baseline_network import AD_baseline_net
 from deepproblog.examples.AD_V0.data.AD_generate_datasets_V1 import get_dataset
 
+from sklearn.metrics import confusion_matrix
+import seaborn as sn
+import pandas as pd
 
+print_rate = 20
 def correct(predictions, labels):
     classes = torch.argmax(predictions, dim=1)
     return torch.mean((classes == labels).float())
 
-train = get_dataset('train')
+
+train_set = get_dataset("train")
+valid_set = get_dataset("train")
+test_set = get_dataset("train")
 
 transform = transforms.Compose(
     [transforms.ToTensor(),
@@ -21,13 +30,14 @@ transform = transforms.Compose(
 
 batch_size = 2
 
-trainloader = torch.utils.data.DataLoader(train, batch_size=batch_size,
+trainloader = torch.utils.data.DataLoader(train_set, batch_size=batch_size,
                                           shuffle=True)
 
-# testloader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size,
-#                                          shuffle=False, num_workers=2)
+validloader = torch.utils.data.DataLoader(valid_set, batch_size=1,
+                                          shuffle=False)
 
-classes = (0,1,2)
+testloader = torch.utils.data.DataLoader(test_set, batch_size=batch_size,
+                                         shuffle=False)
 
 net = AD_baseline_net()
 
@@ -36,11 +46,15 @@ optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.95)
 
 if __name__ == '__main__':
     freeze_support()
-    for epoch in range(20):  # loop over the dataset multiple times
+    epochs = 20
+    accuracy = 0.0
+    running_loss = 0.0
+    correct_var = 0.0
 
-        running_loss = 0.0
-        correct_var = 0
+    for epoch in range(epochs):  # loop over the dataset multiple times
+        print('Epoch-{0} lr: {1}'.format(epoch + 1, optimizer.param_groups[0]['lr']))
         for i, data in enumerate(trainloader, 0):
+
             # get the inputs; data is a list of [inputs, labels]
             inputs, labels = data
 
@@ -49,27 +63,76 @@ if __name__ == '__main__':
 
             # forward + backward + optimize
             outputs = net(inputs)
-
             loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
 
             # print statistics
             running_loss += loss.item()
-            correct_var += correct(outputs, labels)
-            # for i in range(len(labels)):
-            #     correct += int(outputs == labels)
+            # correct_var += correct(outputs, labels)
+            if i % print_rate == 0 and i != 0:  # print every 2000 mini-batches
+
+                for valid_inputs, valid_labels in validloader:
+                    valid_outputs = net(valid_inputs)  # Feed Network
+
+                    valid_outputs = (torch.max(torch.exp(valid_outputs), 1)[1]).data.cpu().numpy()
+                    valid_labels = valid_labels.data.cpu().numpy()
+
+                    if valid_outputs == valid_labels:
+                        correct_var += 1
+
+                print("Iteration:  {}    Average Loss:  {}    Accuracy:  {}".format(epoch * len(trainloader)//print_rate * print_rate + i,
+                                                                                    round(running_loss / 20, 10),
+                                                                                    correct_var / len(validloader)))
+                running_loss = 0.0
+                correct_var = 0.0
+
+        # accuracy = correct_var / len(trainloader)
+        # print("Accuracy = {}".format(accuracy))
 
 
-            # trainset, not train_loader
-            # probably x in your case
+classes = ('Accelerate', 'Brake', 'Idle')
 
 
-            # if i % 20 == 0:    # print every 2000 mini-batches
-            #     print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
-            #     running_loss = 0.0
+def accuracy(matrix, classes):
+    correct = 0
+    for i in range(len(classes)):
+        correct += matrix[i, i]
+    total = matrix.sum()
+    acc = correct / total
+    # print("Accuracy: ", acc)
+    return acc
 
-        accuracy = correct_var / len(trainloader)
-        print("Accuracy = {}".format(accuracy))
 
-print('Finished Training')
+def show_confusion_matrix_baseline(network, data, classes, show_plot=False):
+
+    y_pred = []
+    y_true = []
+
+    # iterate over test data
+    for inputs, labels in data:
+        output = network(inputs)  # Feed Network
+
+        output = (torch.max(torch.exp(output), 1)[1]).data.cpu().numpy()
+        y_pred.extend(output)  # Save Prediction
+
+        labels = labels.data.cpu().numpy()
+        y_true.extend(labels)  # Save Truth
+
+    # constant for classes
+
+
+    # Build confusion matrix
+    cf_matrix = confusion_matrix(y_true, y_pred)
+    df_cm = pd.DataFrame(cf_matrix / np.sum(cf_matrix, axis=1)[:, None], index=[i for i in classes],
+                         columns=[i for i in classes])
+    print(df_cm)
+    print("Accuracy: {}".format(accuracy(cf_matrix, classes)))
+    if show_plot:
+        plt.figure(figsize=(12, 7))
+        sn.heatmap(df_cm, annot=True)
+        plt.show()
+    # plt.savefig('output.png')
+
+show_confusion_matrix_baseline(net, testloader, classes)
+print('DONE')
