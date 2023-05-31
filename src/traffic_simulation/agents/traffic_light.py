28fb19d24ccfad3a3d443a00fc91e7b0081152d3
Ruben <ruben.stabel@student.kuleuven.be>
2023-05-31 16:17:29 +0200

    Add traffic light to simulation

---
+++ src/traffic_simulation/agents/traffic_light.py
@@ -0,0 +1,28 @@
+from traffic_simulation.defs import *
+
+
+class TrafficLight:
+    def __init__(self):
+        self.width = GREEN_LIGHT.get_width()
+        self.height = GREEN_LIGHT.get_height()
+        self.img = None
+        self.position = POSITION_LIGHT
+
+    def set_light(self, color: str):
+        match color:
+            case 'green':
+                self.img = GREEN_LIGHT
+            case 'orange':
+                self.img = ORANGE_LIGHT
+            case 'red':
+                self.img = RED_LIGHT
+
+    def get_light(self):
+        return self.img
+
+    def draw(self, win):
+        if self.img is not None:
+            win.blit(self.img, self.position)
+
+    def reset(self):
+        self.img = GREEN_LIGHT
