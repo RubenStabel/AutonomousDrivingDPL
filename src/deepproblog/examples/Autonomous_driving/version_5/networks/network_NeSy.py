093aa10eae8c3b05a57d7df2e9b29b2d109487ed
Ruben <ruben.stabel@student.kuleuven.be>
2023-08-03 10:30:39 +0200

    [NeSy] - version 5 (models + experiments)

---
+++ src/deepproblog/examples/Autonomous_driving/version_5/networks/network_NeSy.py
@@ -0,0 +1,229 @@
+import torch
+from torch import nn as nn
+import torch.nn.functional as F
+
+
+class AD_V5_NeSy_0_net_danger_pedestrian(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.image_features = nn.Sequential(
+            nn.Conv2d(3, 6, 5),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(6, 16, 5),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.Flatten(0, 2),
+            nn.Linear(16 * 13 * 13, 800),
+            nn.ReLU(),
+            nn.Linear(800, 120),
+            nn.ReLU(),
+            nn.Linear(120, 16),
+            nn.ReLU(),
+        )
+
+        self.numeric_features = nn.Sequential(
+            nn.Linear(1, 9),
+            nn.ReLU(),
+            nn.Linear(9, 9),
+            nn.ReLU(),
+        )
+
+        self.combined_features = nn.Sequential(
+            nn.Linear(25, 16),
+            nn.ReLU(),
+            nn.Linear(16, 4),
+            nn.Softmax(-1)
+        )
+
+    def forward(self, img, spd):
+        img = self.image_features(img)
+        spd = self.numeric_features(spd)
+        z = torch.cat((img, spd), -1)
+        z = self.combined_features(z)
+        return z
+
+
+class AD_V5_NeSy_0_net_speed_zone(nn.Module):
+    def __init__(self, n=8, with_softmax=True, size=16 * 4 * 4):
+        super(AD_V5_NeSy_0_net_speed_zone, self).__init__()
+        self.with_softmax = with_softmax
+        self.size = size
+        if with_softmax:
+            if n == 1:
+                self.softmax = nn.Sigmoid()
+            else:
+                self.softmax = nn.Softmax(1)
+        self.encoder = nn.Sequential(
+            nn.Conv2d(1, 6, 5),
+            nn.MaxPool2d(2, 2),  # 6 24 24 -> 6 12 12
+            nn.ReLU(True),
+            nn.Conv2d(6, 16, 5),  # 6 12 12 -> 16 8 8
+            nn.MaxPool2d(2, 2),  # 16 8 8 -> 16 4 4
+            nn.ReLU(True),
+        )
+        self.classifier = nn.Sequential(
+            nn.Linear(size, 120),
+            nn.ReLU(),
+            nn.Linear(120, 84),
+            nn.ReLU(),
+            nn.Linear(84, n),
+        )
+
+    def forward(self, x):
+        x = self.encoder(x)
+        x = x.view(-1, self.size)
+        x = self.classifier(x)
+        if self.with_softmax:
+            x = self.softmax(x)
+        return x
+
+
+class AD_V5_NeSy_0_net_traffic_light(nn.Module):
+    def __init__(self, n=3, with_softmax=True, size=16 * 13 * 13):
+        super(AD_V5_NeSy_0_net_traffic_light, self).__init__()
+        self.with_softmax = with_softmax
+        self.size = size
+        if with_softmax:
+            if n == 1:
+                self.softmax = nn.Sigmoid()
+            else:
+                self.softmax = nn.Softmax(1)
+        self.encoder = nn.Sequential(
+            nn.Conv2d(3, 6, 5),
+            nn.MaxPool2d(2, 2),  # 6 24 24 -> 6 12 12
+            nn.ReLU(True),
+            nn.Conv2d(6, 16, 5),  # 6 12 12 -> 16 8 8
+            nn.MaxPool2d(2, 2),  # 16 8 8 -> 16 4 4
+            nn.ReLU(True),
+        )
+        self.classifier = nn.Sequential(
+            nn.Linear(size, 800),
+            nn.ReLU(),
+            nn.Linear(800, 80),
+            nn.ReLU(),
+            nn.Linear(80, n),
+        )
+
+    def forward(self, x):
+        x = self.encoder(x)
+        x = x.view(-1, self.size)
+        x = self.classifier(x)
+        if self.with_softmax:
+            x = self.softmax(x)
+        return x
+
+
+class AD_V5_NeSy_0_net_danger_distance(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.image_features = nn.Sequential(
+            nn.Conv2d(3, 6, 5),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(6, 16, 5),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.Flatten(0, 2),
+            nn.Linear(16 * 13 * 13, 800),
+            nn.ReLU(),
+            nn.Linear(800, 120),
+            nn.ReLU(),
+            nn.Linear(120, 16),
+            nn.ReLU(),
+        )
+
+        self.numeric_features = nn.Sequential(
+            nn.Linear(3, 9),
+            nn.ReLU(),
+            nn.Linear(9, 9),
+            nn.ReLU(),
+        )
+
+        self.combined_features = nn.Sequential(
+            nn.Linear(25, 16),
+            nn.ReLU(),
+            nn.Linear(16, 2),
+            nn.Softmax(-1)
+        )
+
+    def forward(self, img, spd, yb, yi):
+        img = self.image_features(img)
+        num = self.numeric_features(torch.cat((spd, yb, yi), -1))
+        z = torch.cat((img, num), -1)
+        z = self.combined_features(z)
+        return z
+
+
+class AD_V5_NeSy_0_net_danger_intersection(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.image_features = nn.Sequential(
+            nn.Conv2d(3, 6, 5),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(6, 16, 5),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.Flatten(0, 2),
+            nn.Linear(16 * 13 * 13, 800),
+            nn.ReLU(),
+            nn.Linear(800, 120),
+            nn.ReLU(),
+            nn.Linear(120, 16),
+            nn.ReLU(),
+        )
+
+        # self.numeric_features = nn.Sequential(
+        #     nn.Linear(1, 2),
+        #     nn.ReLU(),
+        # )
+
+        self.combined_features = nn.Sequential(
+            nn.Linear(17, 8),
+            nn.ReLU(),
+            nn.Linear(8, 2),
+            nn.Softmax(-1)
+        )
+
+    def forward(self, img, pos):
+        img = self.image_features(img)
+        # pos = self.numeric_features(pos)
+        z = torch.cat((img, pos), -1)
+        z = self.combined_features(z)
+        return z
+
+
+class AD_V5_NeSy_0_net_traffic_sign(nn.Module):
+    def __init__(self, n=3, with_softmax=True, size=16 * 13 * 13):
+        super(AD_V5_NeSy_0_net_traffic_sign, self).__init__()
+        self.with_softmax = with_softmax
+        self.size = size
+        if with_softmax:
+            if n == 1:
+                self.softmax = nn.Sigmoid()
+            else:
+                self.softmax = nn.Softmax(1)
+        self.encoder = nn.Sequential(
+            nn.Conv2d(3, 6, 5),
+            nn.MaxPool2d(2, 2),  # 6 24 24 -> 6 12 12
+            nn.ReLU(True),
+            nn.Conv2d(6, 16, 5),  # 6 12 12 -> 16 8 8
+            nn.MaxPool2d(2, 2),  # 16 8 8 -> 16 4 4
+            nn.ReLU(True),
+        )
+        self.classifier = nn.Sequential(
+            nn.Linear(size, 800),
+            nn.ReLU(),
+            nn.Linear(800, 80),
+            nn.ReLU(),
+            nn.Linear(80, n),
+        )
+
+    def forward(self, x):
+        x = self.encoder(x)
+        x = x.view(-1, self.size)
+        x = self.classifier(x)
+        if self.with_softmax:
+            x = self.softmax(x)
+        return x
---
+++ src/deepproblog/examples/Autonomous_driving/version_5/networks/network_baseline.py
@@ -0,0 +1,61 @@
+import torch
+from torch import nn as nn
+import torch.nn.functional as F
+
+
+class AD_V5_baseline_net_0(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 4 * 4, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 8)
+
+        self.image_features = nn.Sequential(
+            nn.Conv2d(3, 6, 5),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(6, 16, 5),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.Flatten(0, 2),
+            nn.Linear(16 * 13 * 13, 800),
+            nn.ReLU(),
+            nn.Linear(800, 120),
+            nn.ReLU(),
+            nn.Linear(120, 16),
+            nn.ReLU(),
+        )
+
+        self.numeric_features = nn.Sequential(
+            nn.Linear(3, 9),
+            nn.ReLU(),
+            nn.Linear(9, 9),
+            nn.ReLU(),
+        )
+
+        self.combined_features = nn.Sequential(
+            nn.Linear(33, 16),
+            nn.ReLU(),
+            nn.Linear(16, 4),
+            nn.Softmax(-1)
+        )
+
+    def mnist(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x)  # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+    def forward(self, img, mnist, spd, ped_y, inter_y):
+        mnist = self.mnist(mnist)
+        img = self.image_features(img)
+        num = self.numeric_features(torch.cat((spd, ped_y, inter_y), -1))
+        z = torch.cat((img, mnist, num), -1),
+        z = self.combined_features(z[0])
+        return z
