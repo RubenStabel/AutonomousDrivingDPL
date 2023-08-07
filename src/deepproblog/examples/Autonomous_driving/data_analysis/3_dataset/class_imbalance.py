4e12a2cc1f1c528f4605fc5407261b63948c2371
Ruben <ruben.stabel@student.kuleuven.be>
2023-08-07 22:49:14 +0200

    [ANALYSIS]  - dataset analysis plots

--- src/deepproblog/examples/Autonomous_driving/data_analysis/3_dataset/class_imbalance.py
+++ src/deepproblog/examples/Autonomous_driving/data_analysis/3_dataset/class_imbalance.py
@@ -1,11 +1,33 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 
-def plot_class_imbalance(acc):
-    plt.plot(acc)
+def plot_class_imbalance(acc_baseline, acc_NeSy, imbalance):
+    plt.plot(imbalance, acc_baseline)
+    plt.plot(imbalance, acc_NeSy)
     plt.ylabel('Accuracy')
     plt.xlabel('Imbalance ratio')
+    # plt.yticks(np.arange(0.55, 0.8, step=0.05))
+    plt.xticks(np.arange(0, 50, step=10))
+    plt.title('Data imbalance')
+    plt.legend(['baseline', 'neuro-symbolic'], loc=7)
     plt.show()
 
 
-acc_baseline = []
+imbalance = [1, 5, 10, 20, 30, 40]
+start_baseline = 0.6890315052508752
+start_NeSy = 0.7311875367430923
+acc_baseline = [0.6890315052508752, 0.6666666666666666, 0.6480990274093722, 0.6221632773356911,
+                0.578544061302682, 0.5481874447391689]
+acc_NeSy = [0.7311875367430923, (0.7284368560494554+0.7424108458591218+0.7257589154140879)/3,
+            (0.7711464780430297+0.6923076923076923)/2, 0.7382847038019452, 0.729000884173298,
+            (0.7305393457117595+0.7015915119363395)/2]
+
+# acc_baseline = [0.6890315052508752-start_baseline, 0.6666666666666666-start_baseline, 0.6480990274093722-start_baseline,
+#                 0.6221632773356911-start_baseline, 0.578544061302682-start_baseline, 0.5481874447391689-start_baseline]
+# acc_NeSy = [0.7311875367430923-start_NeSy, (0.7284368560494554+0.7424108458591218+0.7257589154140879)/3-start_NeSy,
+#             (0.7711464780430297+0.6923076923076923)/2-start_NeSy, 0.7382847038019452-start_NeSy,
+#             0.729000884173298-start_NeSy, (0.7305393457117595+0.7015915119363395)/2-start_NeSy]
+
+
+plot_class_imbalance(acc_baseline, acc_NeSy, imbalance)
---
+++ src/deepproblog/examples/Autonomous_driving/data_analysis/3_dataset/noisy_labels.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def plot_class_imbalance(acc_baseline, acc_NeSy, imbalance):
+    plt.plot(imbalance, acc_baseline)
+    plt.plot(imbalance, acc_NeSy)
+    plt.ylabel('Accuracy')
+    plt.xlabel('Imbalance ratio')
+    # plt.yticks(np.arange(0.95, 0.15, step=0.1))
+    # plt.xticks(np.arange(0, 1, step=0.1))
+    plt.title('Data imbalance')
+    plt.legend(['baseline', 'neuro-symbolic'])
+    plt.show()
+
+
+fraction_noisy_labels = [0, 0.2, 0.4, 0.6, 0.8, 1.0]
+start_baseline = 0.8637358935489304
+start_NeSy = 0.9142664645443827
+acc_baseline = [0.8637358935489304, 0.7481893212059963, 0.6269159508169109,
+                0.4559541856156308, 0.2792656223681994, 0.2594087923193532]
+acc_NeSy = [0.9142664645443827, 0.7945090112851608, 0.707596429172983,
+            0.5805962607377463, (0.427151760148223+0.56088933804952)/2,
+            0.25189152770759642]
+# acc_baseline = [0.8637358935489304-start_baseline, 0.7481893212059963-start_baseline, 0.6269159508169109-start_baseline,
+#                 0.4559541856156308-start_baseline, 0.2792656223681994-start_baseline, 0.2594087923193532-start_baseline]
+# acc_NeSy = [0.9142664645443827-start_NeSy, 0.7945090112851608-start_NeSy, 0.707596429172983-start_NeSy,
+#             0.5805962607377463-start_NeSy, (0.427151760148223+0.56088933804952)/2-start_NeSy,
+#             0.25189152770759642-start_NeSy]
+
+
+plot_class_imbalance(acc_baseline, acc_NeSy, fraction_noisy_labels)
